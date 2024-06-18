cb41d1f41b61995f014a5755eedec00fd440f295
Arpit Gupta <arp.gpt7@gmail.com>
2024-06-19 00:08:11 +0200

    Add logic for handling scoreboard operations

--- src/main/java/com/sports/scoreboard/ScoreboardApp.java
+++ src/main/java/com/sports/scoreboard/ScoreboardApp.java
@@ -1,8 +1,13 @@
 package com.sports.scoreboard;
 
+import com.sports.scoreboard.domain.Game;
 import com.sports.scoreboard.domain.Scoreboard;
 
+import java.util.List;
+
+import static com.sports.scoreboard.operation.ScoreboardOperation.*;
 import static com.sports.scoreboard.util.InputHelper.getIntInput;
+import static com.sports.scoreboard.util.InputHelper.getStringInput;
 
 public class ScoreboardApp {
 
@@ -36,6 +41,31 @@ public class ScoreboardApp {
                 } else if (option == 5) {
                     running = false;
                 } else {
+                    switch (option) {
+                        case 1 -> {
+                            String homeTeamName = getStringInput("Enter home team name: ");
+                            String awayTeamName = getStringInput("Enter away team name: ");
+                            startGame(scoreboard, homeTeamName, awayTeamName);
+                        }
+                        case 2 -> {
+                            int gameId = getIntInput("Enter game id: ");
+                            int homeTeamScore = getIntInput("Enter home team score: ");
+                            int awayTeamScore = getIntInput("Enter away team score: ");
+                            updateGame(scoreboard, gameId, homeTeamScore, awayTeamScore);
+                        }
+                        case 3 -> {
+                            int gameId = getIntInput("Enter game id: ");
+                            finishGame(scoreboard, gameId);
+                        }
+                        case 4 -> {
+                            List<Game> gameList = getSortedGameList(scoreboard);
+                            if (gameList.isEmpty()) {
+                                System.out.println("No active games found.");
+                            } else {
+                                gameList.forEach(System.out::println);
+                            }
+                        }
+                    }
                 }
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
