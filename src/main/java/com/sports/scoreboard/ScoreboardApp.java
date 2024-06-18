33df23535fc6bfda832252c9a9292c77d30b21da
Arpit Gupta <arp.gpt7@gmail.com>
2024-06-18 23:36:50 +0200

    Add main class for ScoreboardApp

---
+++ src/main/java/com/sports/scoreboard/ScoreboardApp.java
@@ -0,0 +1,15 @@
+package com.sports.scoreboard;
+
+public class ScoreboardApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                **********************************************
+                Welcome to the Football World Cup Scoreboard!
+                **********************************************
+                """
+        );
+    }
+}
+
