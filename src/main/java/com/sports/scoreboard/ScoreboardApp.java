fa8dd27fcf5d150495aa65c74a8c1fbe53acc8b2
Arpit Gupta <arp.gpt7@gmail.com>
2024-06-19 00:07:09 +0200

    Show options to user and get input for operation

--- src/main/java/com/sports/scoreboard/ScoreboardApp.java
+++ src/main/java/com/sports/scoreboard/ScoreboardApp.java
@@ -1,5 +1,9 @@
 package com.sports.scoreboard;
 
+import com.sports.scoreboard.domain.Scoreboard;
+
+import static com.sports.scoreboard.util.InputHelper.getIntInput;
+
 public class ScoreboardApp {
 
     public static void main(String[] args) {
@@ -10,6 +14,33 @@ public class ScoreboardApp {
                 **********************************************
                 """
         );
+
+        boolean running = true;
+
+        Scoreboard scoreboard = new Scoreboard();
+
+        while (running) {
+            try {
+                System.out.println("""
+                        1 - Start a game.
+                        2 - Update a game.
+                        3 - Finish a game.
+                        4 - Get live scoreboard.
+                        5 - Exit.
+                        """);
+
+                int option = getIntInput("Choose from the above options: ");
+
+                if (option > 5) {
+                    throw new IllegalArgumentException("Please provide valid option from the given list");
+                } else if (option == 5) {
+                    running = false;
+                } else {
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
 
