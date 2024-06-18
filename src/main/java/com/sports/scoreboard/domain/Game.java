1910d4c1bdf4736efd5c754e56b8cd20c734e9b9
Arpit Gupta <arp.gpt7@gmail.com>
2024-06-18 23:43:46 +0200

    Add constructor to Game class
    with required getters and setters

--- src/main/java/com/sports/scoreboard/domain/Game.java
+++ src/main/java/com/sports/scoreboard/domain/Game.java
@@ -9,4 +9,32 @@ public class Game {
 
     private final LocalDateTime startedTime;
     private LocalDateTime updatedTime;
+
+    public Game(int gameId, String homeTeamName, String awayTeamName) {
+        this.gameId = gameId;
+        this.homeTeam = new Team(homeTeamName);
+        this.awayTeam = new Team(awayTeamName);
+        this.startedTime = LocalDateTime.now();
+        this.updatedTime = LocalDateTime.now();
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public LocalDateTime getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(LocalDateTime updatedTime) {
+        this.updatedTime = updatedTime;
+    }
 }
