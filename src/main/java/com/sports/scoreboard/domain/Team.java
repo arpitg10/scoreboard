df1ade0f1ec026d5458b8d69c50f2e6b3a64a29d
Arpit Gupta <arp.gpt7@gmail.com>
2024-06-18 23:41:23 +0200

    Add constructor to Team class
    With required getter and setters

--- src/main/java/com/sports/scoreboard/domain/Team.java
+++ src/main/java/com/sports/scoreboard/domain/Team.java
@@ -3,4 +3,21 @@ package com.sports.scoreboard.domain;
 public class Team {
     private final String teamName;
     private int score;
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+        this.score = 0;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
