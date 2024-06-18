24ba704dde34e0219d83554b500ff7f99cf41683
Arpit Gupta <arp.gpt7@gmail.com>
2024-06-19 00:09:43 +0200

    Add operation flow to start game
    And return unique gameId

--- src/main/java/com/sports/scoreboard/operation/ScoreboardOperation.java
+++ src/main/java/com/sports/scoreboard/operation/ScoreboardOperation.java
@@ -9,7 +9,9 @@ import java.util.List;
 public class ScoreboardOperation {
 
     public static int startGame(Scoreboard scoreboard, String homeTeamName, String awayTeamName) {
-        return 0;
+        int gameId = scoreboard.startGame(homeTeamName, awayTeamName);
+        System.out.printf("Game started %s - %s, with game ID: %d%n", homeTeamName, awayTeamName, gameId);
+        return gameId;
     }
 
     public static void updateGame(Scoreboard scoreboard, int gameId, int homeTeamScore, int awayTeamScore) {
